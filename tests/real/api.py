d2f85ab5dc34966d8250cb403652f1defc1f64d6
nullJaX <lewanski.jacek@gmail.com>
2024-04-09 00:28:44 +0200

    feat: add requests module

---
+++ tests/real/api.py
@@ -0,0 +1,103 @@
+from typing import Any, AsyncIterator, Dict, Iterator, Optional
+from httpx import AsyncClient, Client
+from ezrest.requests import AsyncConnector, AsyncEndpoint, Connector, Endpoint
+
+JSONType = Dict[str, Any]
+
+
+class ReqResConnector(Connector[JSONType]):
+    client: Client
+
+    def __init__(self) -> None:
+        self.client = Client()
+
+    def _data_request(self, method: str, url: str, data: JSONType = None) -> JSONType:
+        response = getattr(self.client, method)(url, data=data)
+        response.raise_for_status()
+        return response.json()
+
+    def post(self, url: str, data: JSONType = None) -> JSONType:
+        return self._data_request("post", url, data=data)
+
+    def put(self, url: str, data: JSONType = None) -> JSONType:
+        return self._data_request("put", url, data=data)
+
+    def patch(self, url: str, data: JSONType = None) -> JSONType:
+        return self._data_request("patch", url, data=data)
+
+    def delete(self, url: str) -> JSONType:
+        response = self.client.delete(url)
+        response.raise_for_status()
+        return {"code": response.status_code}
+
+    def get(self, url: str, params: Optional[Dict[str, Any]] = None) -> JSONType:
+        params = params or {}
+        response = self.client.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    def list(
+        self, url: str, params: Optional[Dict[str, Any]] = None
+    ) -> Iterator[JSONType]:
+        params = params or {}
+        page: int = int(params.get("page", 0))
+        total_pages: int = page + 1
+        while page < total_pages:
+            response = self.get(url, params=params)
+            page = int(response.get("page", page + 1))
+            total_pages = int(response.get("total_pages", page))
+            for item in response.get("data", []):
+                yield item
+            params["page"] = page + 1
+
+
+class AsyncReqResConnector(AsyncConnector[JSONType]):
+    client: AsyncClient
+
+    def __init__(self) -> None:
+        self.client = AsyncClient()
+
+    async def _data_request(
+        self, method: str, url: str, data: JSONType = None
+    ) -> JSONType:
+        response = await getattr(self.client, method)(url, data=data)
+        response.raise_for_status()
+        return response.json()
+
+    async def post(self, url: str, data: JSONType = None) -> JSONType:
+        return await self._data_request("post", url, data=data)
+
+    async def put(self, url: str, data: JSONType = None) -> JSONType:
+        return await self._data_request("put", url, data=data)
+
+    async def patch(self, url: str, data: JSONType = None) -> JSONType:
+        return await self._data_request("patch", url, data=data)
+
+    async def delete(self, url: str) -> JSONType:
+        response = await self.client.delete(url)
+        response.raise_for_status()
+        return {"code": response.status_code}
+
+    async def get(self, url: str, params: Dict[str, Any] = None) -> JSONType:
+        params = params or {}
+        response = await self.client.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    async def list(
+        self, url: str, params: Dict[str, Any] = None
+    ) -> AsyncIterator[JSONType]:
+        params = params or {}
+        page: int = int(params.get("page", 0))
+        total_pages: int = page + 1
+        while page < total_pages:
+            response = await self.get(url, params=params)
+            page = int(response.get("page", page + 1))
+            total_pages = int(response.get("total_pages", page))
+            for item in response.get("data", []):
+                yield item
+            params["page"] = page + 1
+
+
+ReqResEndpoint = Endpoint[JSONType]
+AsyncReqResEndpoint = AsyncEndpoint[JSONType]
---
+++ tests/real/test_requests.py
@@ -0,0 +1,114 @@
+from httpx import HTTPError
+import pytest
+from tests.real.api import (
+    AsyncReqResConnector,
+    AsyncReqResEndpoint,
+    ReqResConnector,
+    ReqResEndpoint,
+)
+
+BASE_URL = "https://reqres.in/api"
+USER_FIELDS = {"id", "email", "first_name", "last_name", "avatar"}
+
+
+class TestReqResRequests:
+    @pytest.fixture
+    def api(self) -> ReqResEndpoint:
+        return ReqResEndpoint(BASE_URL, ReqResConnector())
+
+    @pytest.mark.parametrize("params", ({}, {"delay": 1}))
+    def test_list_users(self, api: ReqResEndpoint, params):
+        general_response = api.users.get()
+        total_users = int(general_response["total"])
+        user_count = 0
+        for user in api.users.list(params=params):
+            user_count += 1
+            assert all(field in user for field in USER_FIELDS)
+        assert user_count == total_users
+
+    def test_get_user(self, api: ReqResEndpoint):
+        response = api.users[2].get()
+        user_data = response["data"]
+        assert all(field in user_data for field in USER_FIELDS)
+        assert response == api["users"][2].get()
+
+    def test_get_user_not_found(self, api: ReqResEndpoint):
+        with pytest.raises(HTTPError, match="404 Not Found"):
+            api.users[420].get()
+
+    def test_login_via_post(self, api: ReqResEndpoint):
+        post_data = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
+        response = api.login.post(data=post_data)
+        assert "token" in response
+
+    def test_login_via_post_failed(self, api: ReqResEndpoint):
+        post_data = {"email": "peter@klaven"}
+        with pytest.raises(HTTPError, match="400 Bad Request"):
+            api.login.post(data=post_data)
+
+    @pytest.mark.parametrize("method", ("put", "patch"))
+    def test_update(self, api: ReqResEndpoint, method: str):
+        put_data = {"name": "mike", "job": "technician"}
+        response = getattr(api.users[3], method)(data=put_data)
+        for key, value in put_data.items():
+            assert response[key] == value
+        assert "updatedAt" in response
+
+    def test_delete(self, api: ReqResEndpoint):
+        response = api.users[69].delete()
+        assert response["code"] == 204
+
+
+class TestAsyncReqResRequests:
+    @pytest.fixture
+    def api(self) -> AsyncReqResEndpoint:
+        return AsyncReqResEndpoint(BASE_URL, AsyncReqResConnector())
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("params", ({}, {"delay": 1}))
+    async def test_list_users(self, api: AsyncReqResEndpoint, params):
+        general_response = await api.users.get()
+        total_users = int(general_response["total"])
+        user_count = 0
+        async for user in api.users.list(params=params):
+            user_count += 1
+            assert all(field in user for field in USER_FIELDS)
+        assert user_count == total_users
+
+    @pytest.mark.asyncio
+    async def test_get_user(self, api: AsyncReqResEndpoint):
+        response = await api.users[2].get()
+        user_data = response["data"]
+        assert all(field in user_data for field in USER_FIELDS)
+        assert response == await api["users"][2].get()
+
+    @pytest.mark.asyncio
+    async def test_get_user_not_found(self, api: AsyncReqResEndpoint):
+        with pytest.raises(HTTPError, match="404 Not Found"):
+            await api.users[420].get()
+
+    @pytest.mark.asyncio
+    async def test_login_via_post(self, api: AsyncReqResEndpoint):
+        post_data = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
+        response = await api.login.post(data=post_data)
+        assert "token" in response
+
+    @pytest.mark.asyncio
+    async def test_login_via_post_failed(self, api: AsyncReqResEndpoint):
+        post_data = {"email": "peter@klaven"}
+        with pytest.raises(HTTPError, match="400 Bad Request"):
+            await api.login.post(data=post_data)
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("method", ("put", "patch"))
+    async def test_update(self, api: AsyncReqResEndpoint, method: str):
+        put_data = {"name": "mike", "job": "technician"}
+        response = await getattr(api.users[3], method)(data=put_data)
+        for key, value in put_data.items():
+            assert response[key] == value
+        assert "updatedAt" in response
+
+    @pytest.mark.asyncio
+    async def test_delete(self, api: AsyncReqResEndpoint):
+        response = await api.users[69].delete()
+        assert response["code"] == 204
